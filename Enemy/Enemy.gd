48929c2eddbc4646c1ae96d5113821a1b0b1e371
NeoJomoless <92330447+NeoJomoless@users.noreply.github.com>
2023-05-23 20:20:57 -0700

    Add files via upload

---
+++ Enemy/Enemy.gd
@@ -0,0 +1,56 @@
+extends CharacterBody2D
+
+@export var Speed = 40
+@export var health: int = 4
+@export var knockback_strength = 100
+@export var attackpow = 1
+
+@onready var animTree = $AnimationTree
+
+var knockback_dir = Vector2.ZERO
+var knockback = Vector2.ZERO
+var mov_direction = Vector2()
+var see_player = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animTree.active = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if health <= 0:
+		Death()
+	
+	if see_player == true:
+		var player = get_parent().get_node("Player")
+		accelerate_towards_point(player, delta)
+		move_and_slide()
+	else:
+		animTree.get("parameters/playback").travel("Idle")
+	
+	knockback_dir = mov_direction
+	knockback = knockback.move_toward(Vector2.ZERO, 200*delta)
+
+func accelerate_towards_point(point, delta):
+	var movement = mov_direction * Speed
+	mov_direction = (point.position - position).normalized()
+	velocity = movement + (knockback * 2)
+	velocity = velocity.move_toward(mov_direction * Speed, 200 * delta)
+	animTree.get("parameters/playback").travel("Walk")
+	animTree.set("parameters/Idle/blend_position", mov_direction)
+	animTree.set("parameters/Walk/blend_position", mov_direction)
+
+
+
+func _on_attack_detector_body_entered(body):
+	if body.name == "Player":
+		see_player = true
+
+
+func _on_attack_detector_body_exited(body):
+	if body.name == "Player":
+		see_player = false
+
+func Death():
+	queue_free()
---
+++ Player/Player.gd
@@ -0,0 +1,91 @@
+extends CharacterBody2D
+@onready var animated_sprite = $Sprite2D
+@onready var anim = $AnimationPlayer
+@onready var animTree = $AnimationTree
+@onready var AtkTimer = $AttackTimer
+@export var Speed: int = 50
+@export var Friction: float = 0.15
+@export var Acceleration: int = 40
+@export var AtkNumber: int = 3
+@export var Health: int = 4
+@export var attackpow: int = 1
+@export var knockback_strength = 100
+
+var knockback_dir = Vector2.ZERO
+var knockback = Vector2(0, 0)
+var is_moving = false
+var is_attacking = false
+
+func _ready():
+	animTree.active = true
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	if Health <= 0:
+		Death()
+	
+	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
+	var input_vector = Vector2.ZERO
+	if is_attacking == false:
+		input_vector.x = Input.get_action_strength("right") - Input.get_action_strength("left") 
+		input_vector.y = Input.get_action_strength("down") - Input.get_action_strength("up") 
+		
+		velocity = input_vector * Speed + knockback
+		
+		if input_vector == Vector2.ZERO:
+			animTree.get("parameters/playback").travel("Idle")
+		else:
+			knockback_dir = input_vector
+			animTree.get("parameters/playback").travel("Run")
+			animTree.set("parameters/Idle/blend_position", input_vector)
+			animTree.set("parameters/Run/blend_position", input_vector)
+			animTree.set("parameters/Attack1/blend_position", input_vector)
+			animTree.set("parameters/Attack2/blend_position", input_vector)
+			animTree.set("parameters/Attack3/blend_position", input_vector)
+		move()
+	attack_combo()
+
+func attack_combo():
+	if Input.is_action_just_pressed("attack") and AtkNumber == 3:
+		is_attacking = true
+		AtkTimer.start()
+		animTree.get("parameters/playback").travel("Attack1")
+	if Input.is_action_just_pressed("attack") and AtkNumber == 2:
+		is_attacking = true
+		AtkTimer.start()
+		animTree.get("parameters/playback").travel("Attack2")
+	if Input.is_action_just_pressed("attack") and AtkNumber == 1:
+		is_attacking = true
+		AtkTimer.start()
+		animTree.get("parameters/playback").travel("Attack3")
+
+func remove_atknumb():
+	AtkNumber -= 1
+
+func finished_attacking():
+	is_attacking = false
+
+func move():
+	move_and_slide()
+
+
+
+func _on_attack_timer_timeout():
+	AtkNumber = 3
+	is_attacking = false
+
+
+func _on_attack_detector_area_entered(area):
+	if area.is_in_group("Enemy"):
+		Health -= area.get_parent().attackpow
+		knockback = knockback.move_toward(Vector2.ZERO, 100)
+		knockback = area.get_parent().knockback_dir * area.get_parent().knockback_strength
+
+
+func _on_sword_hitbox_area_entered(area):
+	if area.is_in_group("Enemy"):
+		area.get_parent().knockback = knockback_dir * knockback_strength
+		area.get_parent().health -= attackpow
+
+func Death():
+	queue_free()
