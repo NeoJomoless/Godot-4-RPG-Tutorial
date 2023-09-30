0fa80b98fd8793bfe2e7a3aec94f391ea6a9a029
NeoJomoless <92330447+NeoJomoless@users.noreply.github.com>
2023-09-30 11:13:50 -0700

    Add files via upload

--- Enemy/Boss/SpecialStates.gd
+++ Enemy/Boss/SpecialStates.gd
@@ -6,8 +6,29 @@ func Attack1():
 	var Boss = get_parent()
 	Boss.animTree.get("parameters/playback").travel("Special")
 
+func select_random_attack():
+	var rng = RandomNumberGenerator.new()
+	var random_selection = rng.randi_range(0, 2)
+	if random_selection == 0:
+		Attack1()
+	elif random_selection == 1:
+		print("Attack 2")
+	else:
+		print("Attack 3")
+
+func select_attack_conditional():
+	var Boss = get_parent()
+	var max_health = Boss.max_health
+	var curhealth = Boss.health
+	var calchealth: float = float(curhealth) / float(max_health)
+	Attack1()
+	if calchealth <= 0.5:
+		select_random_attack()
+	elif calchealth <= .25:
+		pass
+
 func _on_special_timer_timeout():
 	var Boss = get_parent()
 	if Boss.player_inprox:
 		specialtimer.start()
-		Attack1()
+		select_random_attack()
